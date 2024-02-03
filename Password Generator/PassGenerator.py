bcbaecf42a7e5854ebf0acc4adfe6a486b5da118
hipostajm <hipostajm@gmail.com>
2024-02-03 13:48:42 +0100

    ah yes

--- Password Generator/PassGenerator.py
+++ Password Generator/PassGenerator.py
@@ -4,30 +4,30 @@ SymbolList = ascii_letters + digits + punctuation
 file = open("./Pass.txt", 'w')
 #Its kinda like a zip bomb soo dont use to much for's
 
-for element in SymbolList:
+for element in SymbolList: #282B
 
     Pass=element
 
     file.write(Pass+"\n")
 
-    for element in SymbolList:
+    for element in SymbolList: #34KB
 
         Pass2=Pass+element
 
         file.write(Pass2+"\n")
 
-        for element in SymbolList:
+        for element in SymbolList: #4MB
 
             Pass3=Pass2+element
 
             file.write(Pass3+"\n")
 
-            for element in SymbolList:
+            for element in SymbolList: #450MB
                 Pass4=Pass3+element
 
-                file.write(Pass4+"\n")
+                file.write(Pass4+"\n") 
             
-                for element in SymbolList:
+                for element in SymbolList: #47GB
                     Pass5=Pass4+element
 
                     file.write(Pass5+"\n")
