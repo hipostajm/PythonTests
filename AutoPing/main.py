dca21e3c8aeee6a7e30929bcac660a3ad9f35b3f
hipostajm <u120_tymgor_waw@technischools.com>
2024-02-27 13:31:45 +0100

    fghjkjhgfdfghj

---
+++ AutoPing/main.py
@@ -0,0 +1,41 @@
+import sqlite3
+from ping3 import ping, verbose_ping
+
+con = sqlite3.connect("./AutoPing/AutoPing.db")
+
+cur = con.cursor()
+
+cur.execute('Drop table if exists IPs')
+
+cur.execute('Drop table if exists Ports')
+
+cur.execute("""
+    Create table if not exists IPs(
+            id INTEGER Primary key autoincrement
+            , IP TEXT
+            , Status TEXT
+    )
+""")
+cur.execute("""
+    Create table if not exists Ports(
+            id INTEGER Primary key autoincrement
+            , Port TEXT
+            , Status TEXT
+    )
+""")
+for i in range(0,255):
+
+    ip='192.168.88.'
+    ip+=str(i)
+    r = ping(ip)
+
+    print(ip,' ',r)
+
+    if type(r) == float:
+
+        r = "Is Good"
+
+        cur.execute("Insert into IPs (IP, Status) values (?,?)",[ip,str(r)])
+        con.commit()
+    else:
+        pass
