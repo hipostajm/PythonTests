b84dba2c1730ec057fccccea66ac0bd628324bcd
hipostajm <hipostajm@gmail.com>
2024-03-02 18:32:04 +0100

    grfe

--- Randomizer/main.py
+++ Randomizer/main.py
@@ -10,7 +10,7 @@ file1 = open("./Randomizer/Changed.txt", 'w')
 
 file0 = file0.read().split()
 
-while len(file0)!=0:
+while file0:
     a = random.randint(0,len(file0)-1)
     file1.write(f"{file0[a]} ")
     del file0[a]
