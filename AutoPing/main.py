b33a07f4c8bcd826d7c9bc13d50f68a6af6bd976
hipostajm <145573000+hipostajm@users.noreply.github.com>
2024-03-14 12:11:27 +0100

    Update main.py

--- AutoPing/main.py
+++ AutoPing/main.py
@@ -59,7 +59,7 @@ for i in range(threads_number):
     check += 1
     threads.append(thread)
 
-if threads_number == 1 or 3 or 5 or 15 or 17 or 51 or 85 or 255:
+if threads_number != 1 or 3 or 5 or 15 or 17 or 51 or 85 or 255:
     thread = threading.Thread(target=Scan, args=((int(255/threads_number)*threads_number), 256))
     threads.append(thread)
 
